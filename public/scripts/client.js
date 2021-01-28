db6eee5641ff2009a286e634166fff6afdc58d97
Jacob Allen <jallen2033@gmail.com>
2021-01-28 01:03:16 +0000

    added dynamic fetching of tweets with ajax from the server instead of using hardcoded tweets on the front end

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,32 +3,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function */
 $(document).ready(function(){
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-      {
-        "user": {
-          "name": "Newton",
-          "avatars": "https://i.imgur.com/73hZDYK.png"
-          ,
-          "handle": "@SirIsaac"
-        },
-        "content": {
-          "text": "If I have seen further it is by standing on the shoulders of giants"
-        },
-        "created_at": 1461116232227
-      },
-      {
-        "user": {
-          "name": "Descartes",
-          "avatars": "https://i.imgur.com/nlhLi3I.png",
-          "handle": "@rd" },
-        "content": {
-          "text": "Je pense , donc je suis"
-        },
-        "created_at": 1461113959088
-      }
-    ]
-    
   /* loop through tweets, calls createTweetElement() for each tweet, take the return value & append it to the tweets container
    * grab the formatted return tweets HTML from our createTweetElement() function, update $tweetCollection to scoop up all of the HTML the loop returns
    * then append .incoming-tweets to add that HTML on the end */
@@ -38,6 +12,15 @@ $(document).ready(function(){
       const singleTweet = createTweetElement(tweet);
       $('.incoming-tweets').prepend($(singleTweet));
     }
+  }
+
+  const loadtweets = function() {
+
+    // send ajax request from our client to the server
+    $.ajax({ method: 'GET', url: '/tweets'})
+    .then(function (allTweetsJson) {
+      renderTweets(allTweetsJson);
+    });
   }
       
   /* pass in each object of the data array of objects into here as an incoming paramater
@@ -83,6 +66,21 @@ $(document).ready(function(){
     return html;
   }
   
-  renderTweets(data);
+  // perform ajax call to submit new tweet to the server
+  $('#user-form').on('submit', function(event){
+    
+    // prevent default bahavior of event that fired off on form submit
+    event.preventDefault()
+    const seralizedData = $(this).serialize();
+    console.log("seralizedData: ", seralizedData);
+
+    // send ajax request from our client to the server
+    $.ajax({ method: 'POST', url: '/tweets', data: seralizedData})
+    .then(function () {
+      console.log("Made it");
+    });
+  });
+
+  loadtweets();
 });
 
